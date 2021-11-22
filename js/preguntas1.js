0a8536a200d1527886c859c03e78622adff6d4e8
Adrian2104-A <jlizarragaa1900@alumno.ipn.mx>
2021-11-21 20:25:30 -0600

    Aiudaaaaaa

---
+++ js/preguntas1.js
@@ -0,0 +1,61 @@
+let questions = [
+    {
+        numb: 1,
+        question: "Pregunta 1",
+        answer: "Opción 3",
+        options: [
+            "Opción 1",
+            "Opción 2",
+            "Opción 3",
+            "Opción 4"
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "Pregunta 2",
+        answer: "Opción 4",
+        options: [
+            "Opción 1",
+            "Opción 2",
+            "Opción 3",
+            "Opción 4"
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Pregunta 3",
+        answer: "Opción 2",
+        options: [
+            "Opción 1",
+            "Opción 2",
+            "Opción 3",
+            "Opción 4"
+        ]
+    },
+
+    {
+        numb: 4,
+        question: "Pregunta 4",
+        answer: "Opción 1",
+        options: [
+            "Opción 1",
+            "Opción 2",
+            "Opción 3",
+            "Opción 4"
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Pregunta 5",
+        answer: "Opción 2",
+        options: [
+            "Opción 1",
+            "Opción 2",
+            "Opción 3",
+            "Opción 4"
+        ]
+    },
+];
